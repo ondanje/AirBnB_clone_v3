b890f9604fa9357e69deb66d0a6e25e76b054792
Atala2000 <atalakidi@gmail.com>
2024-01-09 14:10:07 +0300

    Added documentation

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+"""
+Module that provides setup for flask application
+"""
 from flask import Flask, render_template, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import getenv
+import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix="/api/v1")
