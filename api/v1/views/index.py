614628560780a118adf988457d8bb31f53423313
Atala2000 <atalakidi@gmail.com>
2024-01-10 15:10:30 +0300

    changed jsonify return: api/v1/views/index.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -15,14 +15,13 @@ def view_status():
 @app_views.route("/stats", methods=["GET"], strict_slashes=False)
 def get_stats():
     """Retrieves the number of each object by type"""
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User"),
-    }
-    formatted_stats = {f"{key}: {value}" for key, value in stats.items()}
-
-    return jsonify(formatted_stats)
+    return jsonify(
+        {
+            "amenities": storage.count("Amenity"),
+            "cities": storage.count("City"),
+            "places": storage.count("Place"),
+            "reviews": storage.count("Review"),
+            "states": storage.count("State"),
+            "users": storage.count("User"),
+        }
+    )
