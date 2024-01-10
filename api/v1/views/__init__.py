c444769e0449b404c62d65cc97f679d81c543dca
Atala2000 <atalakidi@gmail.com>
2024-01-10 12:54:30 +0300

    changed instantiation of variable: api/v1/views/__init__.py

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,6 +4,8 @@ Module that instanstiates instance of various objects
 """
 from flask import Blueprint
 
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
+
 from api.v1.views.index import *
 from api.v1.views.cities import *
 from api.v1.views.states import *
@@ -12,5 +14,3 @@ from api.v1.views.places import *
 from api.v1.views.states import *
 from api.v1.views.users import *
 from api.v1.views.places_reviews import *
-
-app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
